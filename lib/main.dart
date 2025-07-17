4b6a6872b8268e6da91ae8ab056b4a50655f362f
nawrajjyadav <nawrajjadhav757@gmail.com>
2025-07-17 07:35:59 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,165 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChatApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        backgroundColor: Colors.white,
+        elevation: 1,
+        leading: Icon(Icons.arrow_back, color: Colors.black),
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/avatar.png'), // Replace with your avatar asset
             ),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Jhon Abraham', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16)),
+                Text('Active now', style: TextStyle(color: Colors.grey, fontSize: 12)),
+              ],
+            )
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.call, color: Colors.black)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.videocam, color: Colors.black)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Chip(
+              label: Text('Today'),
+              backgroundColor: Colors.grey[200],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(12),
+              children: [
+                chatBubble('Hello! Jhon Abraham', true, '09:25 AM'),
+                chatBubble('Hello! Nawraj! How are you?', false, '09:25 AM'),
+                chatBubble('You did your job well!', true, '09:26 AM'),
+                chatBubble('Have a great working week!', false, '09:55 AM'),
+                chatBubble('Hope you like it', false, '09:55 AM'),
+                audioMessageBubble('00:16', '09:56 AM'),
+              ],
+            ),
+          ),
+          Divider(height: 1),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            color: Colors.white,
+            child: Row(
+              children: [
+                Icon(Icons.attach_file, color: Colors.grey),
+                SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Write your message',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Icon(Icons.photo, color: Colors.grey),
+                SizedBox(width: 8),
+                Icon(Icons.camera_alt, color: Colors.grey),
+                SizedBox(width: 8),
+                Icon(Icons.mic, color: Colors.grey),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget chatBubble(String message, bool isOutgoing, String time) {
+    return Align(
+      alignment: isOutgoing ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4),
+        padding: EdgeInsets.all(12),
+        constraints: BoxConstraints(maxWidth: 250),
+        decoration: BoxDecoration(
+          color: isOutgoing ? Colors.green[100] : Colors.grey[200],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(isOutgoing ? 12 : 0),
+            bottomRight: Radius.circular(isOutgoing ? 0 : 12),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(message),
+            SizedBox(height: 4),
+            Text(time, style: TextStyle(fontSize: 10, color: Colors.grey)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget audioMessageBubble(String duration, String time) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4),
+        padding: EdgeInsets.all(12),
+        constraints: BoxConstraints(maxWidth: 250),
+        decoration: BoxDecoration(
+          color: Colors.green[100],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+            bottomRight: Radius.circular(0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.play_arrow, size: 20),
+                Expanded(
+                  child: Container(
+                    height: 20,
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    color: Colors.grey,
+                    // Placeholder for waveform visualization
+                  ),
+                ),
+                Text(duration),
+              ],
+            ),
+            SizedBox(height: 4),
+            Text(time, style: TextStyle(fontSize: 10, color: Colors.grey)),
+          ],
+        ),
+      ),
     );
   }
 }
+
